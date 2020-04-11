7d680b5ae762a163288681809d610291530813a1
Mr.Da <836160973da@gmail.com>
2020-04-11 12:13:59 +0800

    管理员登录页面画完

--- apps/blogs-frontend/admin/adminLogin.js
+++ apps/blogs-frontend/admin/adminLogin.js
@@ -10,14 +10,34 @@
 
 var React = require('react');
 
+require('./css/style.css');
+
+
+const loginButtonOnClick = function () {
+    console.log("点击登录");
+};
+
 var AdminLogin = React.createClass({
 
     render: function () {
         console.log("初始化管理员登录页面");
         return(
-           <div>
-               管理员登录
-           </div>
+            <div className="materialContainer">
+                <div className="box">
+                    <div className="title">登录</div>
+                    <div className="input">
+                        <input type="text" name="name" id="name"/>
+                            <span className="spin"></span>
+                    </div>
+                    <div className="button login">
+                        <button onClick={loginButtonOnClick}>
+                            <span>黑入</span>
+                            <i className="fa fa-check"></i>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
         );
     }
 
