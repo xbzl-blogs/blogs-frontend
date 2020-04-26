71c1561938b074d3ab3bc2c5c2488f838a9c76ca
Mr.Da <836160973da@gmail.com>
2020-04-26 16:17:09 +0800

    文章详情修改

--- apps/blogs-frontend/pub/contents/elements/contentElement.js
+++ apps/blogs-frontend/pub/contents/elements/contentElement.js
@@ -9,7 +9,24 @@
  */
 
 var React = require('react');
+import {Link} from "react-router";
 
+
+const setUIADNLI = function (content) {
+    let uidata = [];
+    if(content.li){
+        for (let i in content.li){
+            if(content.li[i].core){
+                uidata.push(<li>{content.li[i].core}</li>);
+            }
+            if(content.li[i].ui){
+                let ul = setUIADNLI(content.li[i].ui);
+                uidata.push(<ul>{ul}</ul>);
+            }
+        }
+    }
+    return uidata;
+};
 
 class ContentElement extends React.Component{
 
@@ -36,13 +53,39 @@ class ContentElement extends React.Component{
                     contents.push(<p>{sortContents[i].content}</p>);
                     break;
                 case 2:
-                    contents.push(<img src={sortContents[i].content}/>);
+                    contents.push(<p><img src={sortContents[i].content}/></p>);
                     break;
                 case 3:
-                    contents.push(<pre><code>{sortContents[i].content}</code></pre>)
+                    contents.push(<pre><code>{sortContents[i].content}</code></pre>);
+                    break;
+                case 6:
+                    let aData = sortContents[i].content.split("<a/>");
+                    let alist = [];
+                    for (let i in aData){
+                        alist.push(<Link >{aData[i]}</Link>);
+                    }
+                    contents.push(<p className="entry__tags"><span>文章 标签</span> <span className="entry__tag-list"> {alist}</span></p>);
+                    break;
+                case 7:
+                    let blockquoteData = sortContents[i].content.split("<blockquote/>");
+                    if(blockquoteData && blockquoteData.length === 2){
+                        contents.push(<blockquote><p>{blockquoteData[0]}</p><cite>{blockquoteData[1]}</cite></blockquote>);
+                    }
+                    break;
+                case 8:
+                    let content = JSON.parse( sortContents[i].content);
+                    let uidata = setUIADNLI(content);
+                    contents.push(<ul >{uidata}</ul>);
                     break;
                 case 9:
-                    contents.push(<p className="lead drop-cap">{sortContents[i].content}</p>)
+                    contents.push(<p className="lead drop-cap">{sortContents[i].content}</p>);
+                    break;
+                case 10:
+                    contents.push(<h2>{sortContents[i].content}</h2>);
+                    break;
+                case 11:
+                    contents.push(<h3>{sortContents[i].content}</h3>);
+                    break;
 
             }
         }
