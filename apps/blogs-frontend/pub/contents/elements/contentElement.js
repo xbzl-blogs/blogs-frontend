419cd9352e0189e04f013db74c775a37c75ab26d
Mr.Da <836160973da@gmail.com>
2020-04-26 14:52:43 +0800

    详情页面修改

--- apps/blogs-frontend/pub/contents/elements/contentElement.js
+++ apps/blogs-frontend/pub/contents/elements/contentElement.js
@@ -4,6 +4,64 @@
  *
  * @date 2020/4/18 16:16
  *
- * 功能：
+ * 功能：content内容 正文部分
  *
  */
+
+var React = require('react');
+
+
+class ContentElement extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            data:props.data,
+            contents:null,
+            haveData: false,
+        };
+    }
+
+    // 组件渲染后调用
+    componentDidMount(){
+        let contents = [];
+        //排序
+        let sortContents = this.state.data.sort(function (a, b) {
+            return parseInt(a.sort) - parseInt(b.sort);
+        });
+
+        for (let i in sortContents){
+            switch (sortContents[i].type) {
+                case 1:
+                    contents.push(<p>{sortContents[i].content}</p>);
+                    break;
+                case 2:
+                    contents.push(<img src={sortContents[i].content}/>);
+                    break;
+                case 3:
+                    contents.push(<pre><code>{sortContents[i].content}</code></pre>)
+                    break;
+                case 9:
+                    contents.push(<p className="lead drop-cap">{sortContents[i].content}</p>)
+
+            }
+        }
+
+        this.setState({
+            contents:contents,
+            haveData:true,
+        });
+    }
+
+    render() {
+        return(
+            this.state.haveData
+                ?   <div className="entry__content">
+                        {this.state.contents}
+                    </div>
+                :   null
+        );
+    }
+}
+
+module.exports = ContentElement;
--- apps/blogs-frontend/pub/contents/elements/headerElement.js
+++ apps/blogs-frontend/pub/contents/elements/headerElement.js
@@ -4,6 +4,46 @@
  *
  * @date 2020/4/18 15:20
  *
- * 功能：
+ * 功能：header内容 标题头
  *
  */
+
+var React = require('react');
+
+
+class HeaderElement extends React.Component{
+
+    constructor(props){
+        super(props);
+        /**
+         * 按照标题存储规则：以<header/>标签分隔内容
+         * 顺序依次是标题、作者、时间、内容分类
+         */
+        let headerData = props.data.content.split("<header/>");
+        this.state = {
+            title:headerData[0],
+            name:headerData[1],
+            date:headerData[2],
+            catefory:headerData[3]
+        };
+    }
+
+    render() {
+        return(
+            <div className="content__page-header entry__header">
+                <h1 className="display-1 entry__title">
+                    {this.state.title}
+                </h1>
+                <ul className="entry__header-meta">
+                    <li className="author">{this.state.name}</li>
+                    <li className="date">{this.state.date}</li>
+                    <li className="cat-links">
+                        {this.state.catefory}
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+module.exports = HeaderElement;
